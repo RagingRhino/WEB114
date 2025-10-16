5700db871d84c814c56632fe4d483c31f45b348a
RagingRhino <55510067+RagingRhino@users.noreply.github.com>
2025-10-15 22:23:55 -0500

    Add files via upload

---
+++ budget/budget.js
@@ -0,0 +1,18 @@
+// Dylan Gregory 10/15/2025
+
+let money = 1000;
+let housing = 100;
+let food = 500;
+let utilities = 225;
+
+let totalIncome = money;
+
+let totalExpenses = housing + food + utilities;
+
+let ramainingMoney = totalIncome - totalExpenses;
+
+console.log( "The total income was: " +   totalIncome.toLocaleString("en-US", {style: "currency", currency: "USD" }));
+console.log( "The rent amount was: " +  housing.toLocaleString("en-US", { style: "currency", currency:"USD" }));
+console.log( "The grocery total was: " +  food.toLocaleString("en-US", { style: "currency", currency:"USD" }));
+console.log( "The percent spent on rent is: " + (housing / money) * 100 + "%");
+console.log( "The percent spent on groceries is: " + (food / money) * 100 + "%");
---
+++ fortune/fortune.js
@@ -0,0 +1,11 @@
+// Dylan Gregory 10/08/2025
+
+let numChildren = 2;
+let partnersName = "Anna";
+let geoLocation = "Kansas";
+let jobTitle = "PCB Designer";
+let salary = 100000;
+
+let concat =`You will be a ${jobTitle} in ${geoLocation}, and married to ${partnersName} with ${numChildren} kids with a salary of ${salary}.`;
+
+console.log(concat);
