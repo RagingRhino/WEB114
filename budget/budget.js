0000753a87df29bf913c725f6680f8ada622cf01
RagingRhino <55510067+RagingRhino@users.noreply.github.com>
2025-10-16 22:35:07 -0500

    Add files via upload

--- budget/budget.js
+++ budget/budget.js
@@ -14,5 +14,5 @@ let ramainingMoney = totalIncome - totalExpenses;
 console.log( "The total income was: " +   totalIncome.toLocaleString("en-US", {style: "currency", currency: "USD" }));
 console.log( "The rent amount was: " +  housing.toLocaleString("en-US", { style: "currency", currency:"USD" }));
 console.log( "The grocery total was: " +  food.toLocaleString("en-US", { style: "currency", currency:"USD" }));
-console.log( "The percent spent on rent is: " + (housing / money) * 100 + "%");
-console.log( "The percent spent on groceries is: " + (food / money) * 100 + "%");
+console.log( `The percent spent on rent is: ${(housing / money) * 100 }%`);
+console.log( `The percent spent on groceries is: ${(food / money) * 100}%`);
